462957e887b2ca44d731229a2cda9a2fe8304915
vagrant <vagrant@precise64.(none)>
2012-11-19 03:16:53 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelMap::Application.config.secret_token = '7892ffb644f9b94662168a5674525094738b89d361fe3870a339de1b9d446004c8037bb2a377d8fea1b92f28de9836f46518194d38a0c2cd7f4818d73b556cc4'
