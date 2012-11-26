2b905d46ba03ab97aa958fb204f3f02a58cf4c44
Tim Watson <tiwatson@gmail.com>
2012-11-26 11:35:59 -0800

    Path

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "bundler/capistrano"
 
 set :shell, '/bin/bash'
-set :default_environment, { 'PATH' => '$PATH:/usr/lib/ruby/gems/1.8/bin/bundle' }
+set :default_environment, { 'PATH' => "/home/watsonswander/.gems/bin:/usr/lib/ruby/gems/1.8/bin/:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games:/usr/lib/ruby/gems/1.8/bin/bundle" }
 set :application, "travel_map"
 set :scm, :git
 set :repository,  "git@github.com:tiwatson/travel_map.git"
