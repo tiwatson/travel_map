a65a6b52005db32160579f97830f148ca56bf58f
Tim Watson <tiwatson@gmail.com>
2012-11-26 11:28:07 -0800

    Path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,5 @@ TravelMap::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
+
+default_environment["PATH"] = "$PATH:/usr/lib/ruby/gems/1.8/bin/"
